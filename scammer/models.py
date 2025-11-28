a5c1835d3881ceb2e2fef50189145d582ea44c74
Alireza <mr.hydarian84@gmail.com>
2025-11-28 10:20:59 -0800

    teset 1

--- scammer/models.py
+++ scammer/models.py
@@ -7,10 +7,6 @@ from django.db import models
 # Create your models here.
 
 
-class Shop(models.Model):
-    name = models.CharField(max_length=250, verbose_name='نام فروشگاه')
-
-
 
 
 # Create your models here.
